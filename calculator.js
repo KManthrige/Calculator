d82912491d82fe545b66e9471db03140e6cf15c9
kmanthrige <k.manthrige@gmail.com>
2023-05-18 14:59:00 -0500

    completed the calculator with limitations. Only 2 numbers will be calculated at a time

--- calculator.js
+++ calculator.js
@@ -1,138 +1,123 @@
-$(document).ready(calculator)
+$(document).ready(function () {
 
-let numbers = [];
-let parameter;
-let equal;
+    let numbers = [];
+    let operator;
+    let result = "";
+    let num = "";
 
-function calculator() {
-    $('.number').click(function () {
-        let value = $(this).text()
-        numbers.push(value)
-        console.log('numbers', numbers)
-
-    }//end click function for nine
-    )//end nine
-
-    // $('#eight').click(function(){
-    //     let value = $(this).attr('value')
-    //     console.log(value)
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#seven').click(function(){
-    //     let value = $(this).attr('value')
-    //     console.log(value)
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#six').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('six')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#five').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('five')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#four').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('four')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#three').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('three')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#two').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('two')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#one').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('one')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#divide').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('divide')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#multiply').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('multipy')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#plus').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('plus')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#minus').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('minus')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#fraction').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('fraction')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#equal').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('equal')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#clear').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('clear')
-    // }//end click function for nine
-    // )//end nine
-
-    // $('#zero').click(function(){
-    //     let value = $(this).attr('class')
-    //     console.log('zero')
-    // }//end click function for nine
-    // )//end nine
-
-    display()
-    calculations()
-}
-
-
-function calculations() {
-    console.log('in calculations')
+    // Function to push parameter to the numbers array and display the number and operator selections
     $('.parameter').click(function () {
         let value = $(this).text()
-        parameter = value
-        console.log('parameter', parameter)
-        if (parameter === "+") {
-            for (i = 0; i < numbers.length; i++){
-                let sum = 0
-                sum += Number(numbers[i])
-                console.log('sum', sum)
-            }//end for
-        }//end if +
-    }//end value function
-    )//end $
-} //end calculations function
+        console.log(value)
+        if (isNaN(value)) {
+            numbers.push(num)
+            num = ""
+            operator = value
+            let el = $('#display')
+            el.append(operator)
+        } else {
+            num = num + value
+            let el = $('#display')
+            el.append(num)
+        }
+    })
 
-function display() {
-    //    let el = $('#answer').val('')
-    //    el.empty()
-    //    el.append(answer)
-}
+    // calculations for 2 numbers only. Display result after clicking the = sign
+    $('.equal').click(function () {
+        numbers.push(num)
+        console.log('numbers', numbers)
+        console.log('operator', operator)
+        if (operator === '+') {
+            result = Number(numbers[0]) + Number(numbers[1])
+        }
+        if (operator === '-') {
+            result = Number(numbers[0]) - Number(numbers[1])
+        }
+        if (operator === '*') {
+            result = Number(numbers[0]) * Number(numbers[1])
+        }
+        if (operator === '/') {
+            result = Number(numbers[0]) / Number(numbers[1])
+        }
+        console.log('result', result)
+
+        let el = $('#display')
+        el.empty()
+        el.append(result)
+
+        numbers = []
+        operator = ""
+        num = ""
+    })
+
+    //clear all the global vairables. 
+    $('.clear').click(function () {
+        numbers = []
+        operator = ""
+        result = ""
+        num = ""
+        let el = $('#display')
+        el.empty()
+    })
+
+})//end 
+
+// let numbers = [];
+// let parameter;
+// let equal;
+// let displayValue = 0
+
+// function calculator() {
+//     $('.number').click(function () {
+//         let value = $(this).text()
+//         numbers.push(value)
+//         console.log('numbers', numbers)
+//     }//end function 
+//     )//end click    
+//     calculatorDisplay()
+//     calculations()
+// }
+
+// function calculations() {
+//     console.log('in calculations')
+//         $('.parameter').click(function () {
+//         let value = $(this).text()
+//         parameter = value
+//         console.log('parameter', parameter)
+//         if (parameter === "+") {
+//             let sum = 0
+//             for (i = 0; i < numbers.length; i++) {
+//                 sum += Number(numbers[i])
+//                 console.log('sum', sum)
+//             }//end for
+//         }//end if +
+//         if (parameter === "-") {
+//             let subt = 0
+//             for (i = 0; i < numbers.length; i++) {
+//                 subt = Number(numbers[0] - numbers[i])
+//                 console.log('subtraction', subt)
+//             }//end for
+//         }//end if -
+//         if (parameter === "*") {
+//             let mult = 0
+//             for (i = 0; i < numbers.length; i++) {
+//                 mult = Number(numbers[0] * numbers[1])
+//                 console.log('multiplication', mult)
+//             }//end for
+//         }//end if *
+//         if (parameter === "/") {
+//             let div = 0
+//             for (i = 0; i < numbers.length; i++) {
+//                 div = Number(numbers[0] / numbers[1])
+//                 console.log('subtraction', div)
+//             }//end for
+//         }//end if -
+//     }//end value function
+//     )//end $
+// } //end calculations function
+
+// function calculatorDisplay() {
+//     let el = document.getElementById('.display')
+//     el.append(displayValue)
+//     return
+// }
